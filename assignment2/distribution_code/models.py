58e612a95ff09659d8239aabea61dcdf5cf22b20
Albert Chalom <albertechalom@gmail.com>
2016-02-27 16:37:39 -0500

    pset 2 code

--- assignment2/distribution_code/models.py
+++ assignment2/distribution_code/models.py
@@ -11,7 +11,7 @@ class Content(object):
 
         # TODO: Delete the following line and replace it with a line
         # that stores the year, month, and day (hint: check out datetime.date)
-        self.creation_date = None
+        self.creation_date = date(year, month, day)
 
         # list of contirbutors
         self.contributors = contributors
@@ -22,6 +22,39 @@ class Content(object):
 
 
 # TODO: Define an Article class that extends the Content class
+class Article(Content):
+
+    def __init__(self, year, month, day, headline, content, contributors):
+        self.headline = headline
+        self.content = content
+        super(Article, self).__init__(year, month, day, contributors)
+
+    def show (self):
+        print "headline: " + self.headline
+        print "content: " + self.content
+        print "contributors: "
+        for contributor in self.contributors:
+            print contributor + " "
+        print "date created " + self.creation_date.isoformat()
 
 
 # TODO: Define a Picture class that extends the Content class
+from PIL import Image
+
+class Picture(Content):
+
+    def __init__(self, year, month, day, title, caption, path, contributors):
+        self.title = title
+        self.caption = caption
+        self.path = path
+        super(Picture, self).__init__(year, month, day, contributors)
+
+    def show (self):
+        print "title: " + self.title
+        print "caption: " + self.caption
+        for contributor in self.contributors:
+            print contributor + " "
+        print "date created " + self.creation_date.isoformat()
+        im = Image.open(self.path)
+        im.show()
+
